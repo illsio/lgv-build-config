25dbad62413c2f8afbb4b1e9a2224ab28e86652e
Weiter <WeiterJo@FHHNET.Stadt.Hamburg.de>
2016-07-20 15:58:46 +0200

    update grunt test repariert

--- tasks/connect.js
+++ tasks/connect.js
@@ -6,7 +6,29 @@
 module.exports = {
     test: {
         options: {
-            port: 8001
+            port: 8001,
+            keepalive: true,
+            middleware: function (connect, options) {
+            if (!Array.isArray(options.base)) {
+                options.base = [options.base];
+            }
+
+            // Setup the proxy
+            var middlewares = [require("grunt-connect-proxy/lib/utils").proxyRequest];
+
+            // Serve static files.
+            options.base.forEach(function (base) {
+                middlewares.push(connect.static(base));
+            });
+
+            // Make directory browse-able.
+            var directory = options.directory || options.base[options.base.length - 1];
+
+            middlewares.push(connect.directory(directory));
+
+            return middlewares;
+        }
+
         }
     },
 
