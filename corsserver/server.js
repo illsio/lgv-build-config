7cac412192beb0b56143d67c0b9a712ef427d914
mbgvhh <markus.besenfelder@gv.hamburg.de>
2017-01-20 12:16:54 +0100

    update corsserver nutzt urlrewrite, damit die css-Pfade wie im gebauten Portal funktionieren

--- corsserver/server.js
+++ corsserver/server.js
@@ -6,6 +6,8 @@ app.use(cors({}));
 
 // server bietet diese statischen Dateien unter dem Pfad /lgv-cors an; Pfad ist relativ zu dieser Datei
 app.use("/lgv-cors", express.static("../../../"));
+app.use(rewrite('/lgv-cors/fonts/*', '/lgv-cors/components/bootstrap/fonts/$1'));
+app.use(rewrite('/lgv-cors/css/woffs/*', '/lgv-cors/components/lgv-config/css/woffs/$1'));
 
 app.listen(3000, function () {
     console.log("Example app listening on port 3000!");
